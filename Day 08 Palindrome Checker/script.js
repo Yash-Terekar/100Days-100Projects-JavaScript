4103dbe58bee3a75f90775b6a6c73120290b2337
Yash <yashterekar26@gmail.com>
2025-07-23 18:17:45 +0530

    Day 8 Palindrome Checker

---
+++ Day 08 Palindrome Checker/script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("input");
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".output");
+
+function checkPalindrome() {
+  let num = input.value.trim();
+
+  if (num == "") {
+    alert("Enter a Number");
+    result.textContent = "Enter a Number";
+    return;
+  }
+
+  num = parseInt(num);
+
+  if (isNaN(num)) {
+    alert("Please Enter a Valid Number");
+    return;
+  }
+
+  let reverseNum = num.toString().split("").reverse().join("");
+
+  if (num == reverseNum) {
+    result.textContent = `${num} is Palindrome Number`;
+  } else {
+    result.textContent = `${num} is not a Palindrome`;
+  }
+}
+btn.addEventListener("click", checkPalindrome);
