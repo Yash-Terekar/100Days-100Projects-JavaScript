efc84c0ec7b57d347fabd033c3e277f038ecaaa0
Yash <yashterekar26@gmail.com>
2025-07-28 21:28:41 +0530

    Init Day 12 Image Slider

---
+++ Day 12 Image Slider/script.js
@@ -0,0 +1,7 @@
+const images = [
+  "https://images.pexels.com/photos/32542488/pexels-photo-32542488.jpeg",
+  "https://images.pexels.com/photos/32542560/pexels-photo-32542560.jpeg",
+  "https://images.pexels.com/photos/32542489/pexels-photo-32542489.jpeg",
+  "https://images.pexels.com/photos/32542556/pexels-photo-32542556.jpeg",
+  "https://images.pexels.com/photos/32542556/pexels-photo-32542556.jpeg",
+];
